b8c261446dc1286da89c8f8f1d94f8939d7e4f4b
PIER0318 <lefebvre.pierre0318@gmail.com>
2021-09-17 09:33:41 -0400

    crawler fonctionnel

--- crawler.py
+++ crawler.py
@@ -8,7 +8,17 @@ import re
 main_page = requests.get('https://aonprd.com/Spells.aspx?Class=All')
 soup = BeautifulSoup(main_page.content, 'lxml')
 
-for link in soup.find_all('a')[98:110]: #[105:108] #3137
+spell = {
+        "name": "NA",
+        "level": "NA",
+        "components": "NA",
+        "spell_resistance": False,
+    }
+
+spells = []
+cpt = 0
+
+for link in soup.find_all('a')[98:3137]: #[105:108] #3137
     page = requests.get('https://aonprd.com/'+link.get('href'))
     soup_bis = BeautifulSoup(page.content, 'lxml')
     table_html = soup_bis.find_all('table')[-1]
@@ -24,25 +34,37 @@ for link in soup.find_all('a')[98:110]: #[105:108] #3137
         except:
             pass
         try:
-            print("name:", spans_html[i].find_all("h1")[i].text).strip()
+            spell["name"] = spans_html[i].find_all("h1")[i].text.strip()
+            #print("name:", spans_html[i].find_all("h1")[i].text.strip())
         except:
-            pass
+            spell["name"] = "NA"
         try:
             if (findnth(span, 'Level', i) != -1):
-                print("level:", span[findnth(span, 'Level ', i)+6:findnth(span, 'Casting', 2*i)]) # la bidouille ~(-_-)~
+                spell["level"] = span[findnth(span, 'Level ', i)+6:findnth(span, 'Casting', 2*i)]
+                #print("level:", span[findnth(span, 'Level ', i)+6:findnth(span, 'Casting', 2*i)]) # la bidouille ~(-_-)~
         except:
-            pass
+            spell["level"] = "NA"
         try:
             if (findnth(span, 'Components', i) != -1):
-                print("components:", span[findnth(span, 'Components ', i)+11:findnth(span, 'EffectRange', i)])
+                spell["components"] = span[findnth(span, 'Components ', i)+11:findnth(span, 'EffectRange', i)]
+                #print("components:", span[findnth(span, 'Components ', i)+11:findnth(span, 'EffectRange', i)])
         except:
-            pass
+            spell["components"] = "NA"
         try:
             if (findnth(span, 'Components', i) != -1):
-                print("spell_resistance:", True if ("Spell Resistance" in span[findnth(span, 'Effect', i):findnth(span, 'Description', i)]) else False)
-                print("\n")
+                spell["spell_resistance"] = True if ("Spell Resistance" in span[findnth(span, 'Effect', i):findnth(span, 'Description', i)]) else False
+                #print("spell_resistance:", True if ("Spell Resistance" in span[findnth(span, 'Effect', i):findnth(span, 'Description', i)]) else False, "\n")
         except:
-            pass
+            spell["spell_resistance"] = False
 
+        if(spell["name"] != "NA"):
+            spells.append(spell.copy())
 
+    cpt += 1
+    if(cpt%50 == 0):
+        print(round(cpt/(3137-98)*100),'%')
 
+print(round(cpt/(3137-98)*100),'%')
+
+with open('C:/Users/user/Desktop/UQAC_3A/8INF803_Base_de_donnée_répartie/TP_BDR/TP1_BDR/results/result.json', 'w') as fout:
+    fout.write('[' + ',\n'.join(json.dumps(i) for i in spells) + ']\n')
