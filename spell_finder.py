739640d5b9874d9412c9b3d270c2fc3a6b6d66bc
PIER0318 <lefebvre.pierre0318@gmail.com>
2021-09-24 10:37:52 -0400

    spell_finder.py V2

--- spell_finder.py
+++ spell_finder.py
@@ -1,10 +1,29 @@
+from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
+import json
 
-conf = SparkConf().setAppName('MyFirstStandaloneApp')
+conf = SparkConf().setAppName('Spell Finder')
 sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
+spark = SparkSession(sc)
 
-rdd = sc.parallelize("./results/result.json")
+data = json.load(open("C:/Users/user/Desktop/UQAC_3A/8INF803_Base_de_donnée_répartie/TP_BDR/TP1_BDR/results/result.json"))
+rdd = sc.parallelize(data)
 df = rdd.toDF()
+"""
+df.printSchema()
+df.show(truncate=False)
+"""
+result = df.filter(df.level.contains("wizard 1")
+                   | df.level.contains("wizard 2")
+                   | df.level.contains("wizard 3")
+                   | df.level.contains("wizard 4")).collect()
+print(result)
+
+
+
+
+"""
 result = df.filter(df.level <= 4).collect()
 print(result)
+"""
