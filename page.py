e67f32489e3b2a9ecaf44cfd1a41f23538b17e35
Vincent Gagnon <vincentgagnon53@gmail.com>
2021-10-22 15:12:16 -0400

    Added pagerank algorithm

---
+++ page.py
@@ -0,0 +1,13 @@
+class Page:
+    def __init__(self, name, neighbors, page_rank):
+        self.name = name
+        self.neighbors = neighbors
+        self.pageRank = page_rank
+
+    def send_messages(self):
+        tab = []
+        for neighbor in self.neighbors:
+            #neighbor_number = 1 if len(self.neighbor) == 0 else len(self.neighbors)
+            tab.append((neighbor, (self.pageRank / len(self.neighbors))))
+            tab.append((self.name, 0))
+        return tab
